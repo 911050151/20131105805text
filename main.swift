ba26e76f1ff383bda3282c66f1f41bd0383800ac
911050151 <20131105805lijianqing@bieliangewifi.local>
2016-03-10 11:52:22 +0800

    index
    区间的循环 访问数组

---
+++ main.swift
@@ -0,0 +1,43 @@
+//
+//  main.swift
+//  20131105805text
+//
+//  Created by 20131105805李建青 on 16/3/10.
+//  Copyright © 2016年 20131105805李建青. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+/*
+var i:Int
+i=0
+while i<10 {
+    print("\(i)")
+    i++
+}
+*/
+//浮点数
+/*
+var oneThirdInFlont:Float=1/3
+var oneThirdInDouble:Double=1/3
+print(oneThirdInFlont)
+print(oneThirdInDouble)
+*/
+
+
+/*
+for index in 1...5{
+    print(index)
+}
+*/
+/*
+var index:Int
+for index=0;index<3;++index{
+    print("index is \(index)")
+}
+*/
+var someInts=[Int](count:3,repeatedValue:10)
+var someVar=someInts[0]
+print("Value of first elementis \(someVar)")
+print("Value of second elementis \(someInts[1])")
+print("Value of third elementis \(someInts[2])")
